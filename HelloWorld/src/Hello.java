e9589927cc849331b781fbcc82bebd537dec1064
mayragann <mayragann@gmail.com>
2022-12-12 14:05:50 -0500

    More examples on if statements on Java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -8,5 +8,14 @@ public class Hello {
         } else {
             System.out.println("It's always good to learn new things!");
         }
+        int topScore = 50;
+        if (topScore < 100){
+            System.out.println("They are winning!");
+        }
+
+        int secondTopScore = 55;
+        if (secondTopScore > topScore && topScore < 100){
+            System.out.print("Greater than top score and less than 100!");
+        }
     }
 }
