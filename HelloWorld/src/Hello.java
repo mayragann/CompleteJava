653f488f8717087ae6230f014dc4a1c79bc1276d
mayragann <mayragann@gmail.com>
2022-12-12 13:59:15 -0500

    Added Program initial problem

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Person");
+
+        boolean isAlwaysLearning = true;
+        if (!isAlwaysLearning) {
+            System.out.println("Lets get learning!");
+        } else {
+            System.out.println("It's always good to learn new things!");
+        }
+    }
+}
