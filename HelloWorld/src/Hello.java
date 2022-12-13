8869d45499560e2e6bab4ebc38518f8f689f2663
mayragann <mayragann@gmail.com>
2022-12-13 07:17:34 -0500

    Added additional examples

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -8,14 +8,43 @@ public class Hello {
         } else {
             System.out.println("It's always good to learn new things!");
         }
-        int topScore = 50;
+/*
+        int topScore = 80;
         if (topScore < 100){
             System.out.println("They are winning!");
         }
 
-        int secondTopScore = 55;
-        if (secondTopScore > topScore && topScore < 100){
+        int secondTopScore = 95;
+        if ((secondTopScore > topScore) && (topScore < 100)){
             System.out.print("Greater than top score and less than 100!");
         }
+        if ((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+*/
+
+        int newValue = 50;
+        if (newValue == 50){
+            System.out.println("This is not an error");
+        }
+        boolean isCar = false;
+        if (isCar == true){
+            System.out.println("This isn't supposed to happen");
+        }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar != "Volkswagen";
+
+        if (isDomestic){
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+        System.out.println(s);
+        
+        int ageOfClient = 16;
+        String ageText = (ageOfClient >= 18) ? "Over Eighteen" : "Still a kid";
+        System.out.println("Our Client is " + ageOfClient);
     }
+
 }
