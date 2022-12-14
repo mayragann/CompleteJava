54a7d94c9facf05937916a651491079561a63cd8
mayragann <mayragann@gmail.com>
2022-12-14 08:21:01 -0500

    Another challenge completed and added

--- FirstCodingExercise/src/WeightConversion.java
+++ FirstCodingExercise/src/WeightConversion.java
@@ -1,6 +1,21 @@
 public class WeightConversion {
     public static void main(String[] args) {
-
+        printConversion(200);
+    }
+    public static void printConversion(double kilosToPounds){
+        if(kilosToPounds < 0){
+            System.out.println("Invalid Weight");
+        }
+        else{
+            System.out.println(kilosToPounds + "lbs is = " + toKilosToPounds(kilosToPounds) + "kgs.");
+        }
     }
 
+    public static long toKilosToPounds (double kilosToPounds){
+        if(kilosToPounds < 0){
+            return -1;
+        } else {
+            return Math.round(kilosToPounds/2.2);
+        }
+    }
 }
